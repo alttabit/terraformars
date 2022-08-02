6c6c5936f4b142d4109cf133427a0bbbbdeb5098
alttabit <mitchellcook@developersinstitue.co.nz>
2022-08-02 14:09:11 +1200

    test

---
+++ .terraform.lock.hcl
@@ -0,0 +1,24 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/okta/okta" {
+  version     = "3.32.0"
+  constraints = "~> 3.10"
+  hashes = [
+    "h1:qFElNvaY7hUAg5+CW1eg5Tew8OMHxwfGxZJRLDrEQEg=",
+    "zh:03e2d50796bceff315522203479b04f43c8712c6d8aa93608eb7f3b90b60c330",
+    "zh:103e540d4e8286b3a42320ee0c98cb213abaa3106971e401cf1b03cedb9afd21",
+    "zh:127a6eab3121b579983c316c39cec9050d0ddd8b70f1c7b7098dfc0afa4f0b7e",
+    "zh:27895460efbcd269e39ed78797edfeeba322bffcc64a1b707ef04fd62ceb13d8",
+    "zh:3a1604b0a29a8c320113d5c1a48451b475350a6c9650a737b51c2eba81d7045e",
+    "zh:5bb0c6759bc01112fd00d0469def47a7fb22a2cddfe16b3347bd7c0a853439a6",
+    "zh:5e985c60169cef839a3b32186043014c6ad395a7f922e24fab183e7acdb2d447",
+    "zh:607614fda639587e6096713c24aff0962e649fcb6f15777c1709dedab29d5778",
+    "zh:71015591779909067fc8e962824bf6167518fce38087f43e2f071b3eb1d2bf6c",
+    "zh:a15b90e70597a239e32ef959e73a5bcd7fc4809da885c9c0e7bdac01960307c4",
+    "zh:a863aa002cf34356dd4ede8afc56ca7baf1ce79e374258e7bb8156c6ebd6e80e",
+    "zh:ac6cd1498077da3c1df01d1ca6ac9ffaa680ac11ca858c6e9b08fed14226c6dd",
+    "zh:c2e762d70fab9c32ba9644724e3515ae86b5dae4399019c6aea3ec7a9a728158",
+    "zh:f5c1a6e30112c7d23381cd730fc08af974ff5246d1b2aeea3a6a6ea9a097dbab",
+  ]
+}
--- main.tf
+++ main.tf
@@ -1,38 +1,28 @@
-# The following configuration uses a provider which provisions [fake] resources
-# to a fictitious cloud vendor called "Fake Web Services". Configuration for
-# the fakewebservices provider can be found in provider.tf.
-#
-# After running the setup script (./scripts/setup.sh), feel free to change these
-# resources and 'terraform apply' as much as you'd like! These resources are
-# purely for demonstration and created in Terraform Cloud, scoped to your TFC
-# user account.
-#
-# To review the provider and documentation for the available resources and
-# schemas, see: https://registry.terraform.io/providers/hashicorp/fakewebservices
-#
-# If you're looking for the configuration for the remote backend, you can find that
-# in backend.tf.
-
-
-resource "fakewebservices_vpc" "primary_vpc" {
-  name       = "Primary VPC"
-  cidr_block = "0.0.0.0/1"
+terraform {
+  required_providers {
+    okta = {
+      source = "okta/okta"
+      version = "~> 3.10"
+    }
+  }
 }
 
-resource "fakewebservices_server" "servers" {
-  count = 2
-
-  name = "Server ${count.index + 1}"
-  type = "t2.micro"
-  vpc  = fakewebservices_vpc.primary_vpc.name
+# Configure the Okta Provider - API token set in env var.
+provider "okta" {
+  org_name  = "dev-00369028"
+  api_token = "005ukqpj32AnM_jS748vd6AsWrOM295fSeqXze-Ont"
 }
 
-resource "fakewebservices_load_balancer" "primary_lb" {
-  name    = "Primary Load Balancer"
-  servers = fakewebservices_server.servers[*].name
+resource "okta_user" "terraform_application" {
+  login = "svc_terraform@example.com"
+  email = "svc_terraform@example.com"
+  first_name = "Terraform"
+  last_name = "Application Automation"
+  admin_roles = [ "APP_ADMIN" ]
 }
 
-resource "fakewebservices_database" "prod_db" {
-  name = "Production DB"
-  size = 256
+resource "okta_group" "example_skip" {
+  name        = "Example"
+  description = "My Example Group"
+  skip_users  = true
 }
