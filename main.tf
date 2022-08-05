3d9fd708dc18bca3ced9d41ec9b22dddaf6e0bb8
alttabit <mitchellcook@developersinstitue.co.nz>
2022-08-05 15:44:13 +1200

    f

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "okta_group_rule" "staff" {
 
 resource "okta_app_saml" "Atlassian_Cloud" {
   label                    = "Atlassian Cloud"
-  preconfigured_app        ="Atlassian_Cloud"
+  preconfigured_app        ="atlassian cloud"
   sso_url                  = "https://dev-00369028.okta.com"
   recipient                = "https://nextek.atlassian.net"
   destination              = "https://nextek.atlassian.net/wiki/home"
