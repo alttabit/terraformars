81d15f5d9f6010bfb8a4c4774f032f1cea312d98
alttabit <mitchellcook@developersinstitue.co.nz>
2022-08-11 21:44:03 +1200

    h

--- main.tf
+++ main.tf
@@ -63,10 +63,4 @@ resource "okta_app_saml" "staff_workspace" {
   saml_version      = "2.0"
 }
 
-resource "okta_app_saml" "learner_workspace" {
-  preconfigured_app = "google"
-  label             = "Learner Google Workspace"
-  status            = "ACTIVE"
-  saml_version      = "2.0"
-}
 
