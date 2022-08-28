12754e68852ab8263da9841b3db43250e1bbb0c4
alttabit <mitchellcook@developersinstitue.co.nz>
2022-08-28 22:26:45 +1200

    d

--- main.tf
+++ main.tf
@@ -39,24 +39,15 @@ resource "okta_group" "Staff" {
 
 #apps
 
-
-
 resource "okta_app_saml" "learner2_google_workspace" {
 preconfigured_app = "google"
   label             = "lear23wre google"
   status            = "ACTIVE"
   saml_version      = "2.0"
-  domain = "uverse0.com"
-    sso_url                  = "https://example.com"
-  recipient                = "https://example.com"
-  destination              = "https://example.com"
-  audience                 = "https://example.com/audience"
-    subject_name_id_template = "$${user.userName}"
-  subject_name_id_format   = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
-  response_signed          = true
-  signature_algorithm      = "RSA_SHA256"
-  digest_algorithm         = "SHA256"
-  honor_force_authn        = false
-  authn_context_class_ref  = "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport"
-
+  app_settings_json = <<JSON
+{
+    "domain": "okta",
+    "afwOnly": false
+}
+JSON
 }
